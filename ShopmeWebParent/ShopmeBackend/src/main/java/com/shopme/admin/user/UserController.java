a8304853af2bfe276d12c30fc378f4f127b4a316
Hoai Phong <19110262@student.hcmute.edu.vn>
2021-12-26 22:04:44 +0700

    Finaly pagination course 37

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -26,22 +26,26 @@ public class UserController {
 	private UserService service;
 
 	@GetMapping("/users")
-	public String listAll(Model model) {
-		List<User> listUsers = service.listAll();
-		model.addAttribute("listUsers", listUsers);
-		return "users";
+	public String listFirstPage(Model model) {
+		return listByPage(1, model);
 	}
 
 	@GetMapping("/users/page/{pageNumber}")
 	public String listByPage(@PathVariable(name = "pageNumber") int pageNum, Model model) {
 		Page<User> page = service.listByPage(pageNum);
 		List<User> listUsers = page.getContent();
-
-		System.out.println("Pagenum = " + pageNum);
-		System.out.println("Total elements = " + page.getTotalElements());
-		System.out.println("Total pages = " + page.getTotalPages());
+		
+		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
+		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
 		model.addAttribute("listUsers", listUsers);
-
 		return "users";
 	}
 
