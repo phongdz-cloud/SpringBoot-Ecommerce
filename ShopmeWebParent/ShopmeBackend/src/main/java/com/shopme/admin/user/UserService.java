c15bf9f1fa4ee82842ba40970daab89319fbb168
Hoai Phong <19110262@student.hcmute.edu.vn>
2021-12-26 22:51:09 +0700

    sort pagition course 39

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,8 +32,11 @@ public class UserService {
 		return (List<User>) userRepo.findAll();
 	}
 
-	public Page<User> listByPage(int pageNum) {
-		PageRequest pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE);
+	public Page<User> listByPage(int pageNum, String sortField, String sortDir) {
+		Sort sort = Sort.by(sortField);
+
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+		PageRequest pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE, sort);
 		return userRepo.findAll(pageable);
 	}
 
