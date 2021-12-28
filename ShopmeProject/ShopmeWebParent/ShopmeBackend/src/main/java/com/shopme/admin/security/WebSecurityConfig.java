159857352acf9de38276566cad32e0ede598377e
Hoai Phong <19110262@student.hcmute.edu.vn>
2021-12-28 10:25:57 +0700

    spring security remember me course 53

--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -47,7 +47,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginPage("/login")
 				.usernameParameter("email")
 				.permitAll()
-			.and().logout().permitAll();
+			.and().logout().permitAll()
+			.and().
+				rememberMe()
+					.key("AbcDefgHijKlmnOpqrs_1234567890")
+					.tokenValiditySeconds(7 * 24 * 60 * 60);
 			
 	}
 
