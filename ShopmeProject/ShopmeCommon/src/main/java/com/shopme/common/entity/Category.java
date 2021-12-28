9d1c7b12ba0d7822b6c2a4aa66733fb905b8a83f
Hoai Phong <19110262@student.hcmute.edu.vn>
2021-12-28 16:39:51 +0700

    Code Unit Test For Category course 66

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -14,29 +14,47 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name ="categories")
+@Table(name = "categories")
 public class Category {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Column(length = 128, nullable = false, unique = true)
 	private String name;
-	
+
 	@Column(length = 64, nullable = false, unique = true)
 	private String alias;
-	
+
 	@Column(length = 128, nullable = false)
 	private String image;
-	
+
 	private boolean enabled;
-	
+
 	@OneToOne
 	@JoinColumn(name = "parent_id")
 	private Category parent;
-	
+
 	@OneToMany(mappedBy = "parent")
 	private Set<Category> children = new HashSet<>();
+
+	public Category() {
+	}
+
+	public Category(Integer id) {
+		this.id = id;
+	}
+
+	public Category(String name) {
+		this.name = name;
+		this.alias = name;
+		this.image = "default.png";
+	}
+
+	public Category(String name, Category parent) {
+		this(name);
+		this.parent = parent;
+	}
 
 	public Integer getId() {
 		return id;
@@ -93,6 +111,5 @@ public class Category {
 	public void setChildren(Set<Category> children) {
 		this.children = children;
 	}
-	
-	
+
 }
