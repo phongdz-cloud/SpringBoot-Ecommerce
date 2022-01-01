916ba3f846b703eddfd6336776e4c0662d591d6b
Hoai Phong <19110262@student.hcmute.edu.vn>
2022-01-01 14:28:17 +0700

    Code logo finally!

--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/MvcConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/MvcConfig.java
@@ -6,27 +6,23 @@ import java.nio.file.Paths;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String dirName= "user-photos";
-		Path userPhotoDir= Paths.get(dirName);
-		
-		String userPhotosPath= userPhotoDir.toFile().getAbsolutePath();
-		
-		registry.addResourceHandler("/"+dirName+"/**")
-			.addResourceLocations("file:/"+userPhotosPath +"/");
-		
-		String categoryImagesDirName= "../category-images";
-		Path categoryImagesDir= Paths.get(categoryImagesDirName);
-		
-		String categoryImagesPath= categoryImagesDir.toFile().getAbsolutePath();
-		
-		registry.addResourceHandler("/category-images/**")
-			.addResourceLocations("file:/"+categoryImagesPath +"/");
+		exposeDirectory("user-photos", registry);
+		exposeDirectory("../category-images", registry);
+		exposeDirectory("../brand-logos", registry);
 	}
 
-	
+	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+		Path path = Paths.get(pathPattern);
+		String absolutePath = path.toFile().getAbsolutePath();
+
+		String logicalPath = pathPattern.replace("../", "") + "/**";
+
+		registry.addResourceHandler(logicalPath).addResourceLocations("file:/" + absolutePath + "/");
+	}
 }
--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/category/CategoryRestController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/category/CategoryRestController.java
@@ -16,4 +16,5 @@ public class CategoryRestController {
 			@Param("alias") String alias) {	
 		return service.checkUnique(id, name, alias);
 	}
+	
 }
